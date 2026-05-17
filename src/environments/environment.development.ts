006ae185fd56d1b0e79a846ea44709f4cac368bb
Joao Guilherme Mathias Alves <joaoguilhermeoboista@gmail.com>
2026-05-17 08:07:19 -0300

    mudança

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Ajuste a porta (5000, 7000, etc) para a mesma que aparece no seu Swagger/C#
-  apiUrl: 'http://zeroonecurso-001-site1.ltempurl.com/api' 
+  apiUrl: 'https://zeroonecurso-001-site1.ltempurl.com/api' 
 };
