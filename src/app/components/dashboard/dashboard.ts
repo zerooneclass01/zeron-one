4c875ece61765017f205862793abb250545d29f8
Joao Guilherme Mathias Alves <joaoguilhermeoboista@gmail.com>
2026-05-18 19:20:58 -0300

    correção na segurança

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
 import { Component, signal, OnInit, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common'; // Importação essencial
+import { isPlatformBrowser } from '@angular/common'; 
+import { AuthService } from '../../services/AuthService';
 import { Router, RouterModule } from '@angular/router';@Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -8,11 +9,14 @@ import { Router, RouterModule } from '@angular/router';@Component({
   styleUrls: ['./dashboard.css']
 })
 
+
+
 export class Dashboard implements OnInit {
   // 1. Injete o PLATFORM_ID para identificar onde o código está rodando
   constructor(
     private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private authService: AuthService
   ) { }
 
   userRole = signal<number | null>(null);
@@ -23,6 +27,15 @@ export class Dashboard implements OnInit {
       const role = localStorage.getItem('user_role');
       this.userRole.set(role !== null ? Number(role) : null);
     }
+  }
+
+  executarLogout() {
+    this.authService.logout(); 
+    
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('user_role');
+    }
+    this.router.navigate(['/']); 
   }
 
   podeAcessarAdministrativoERh() {
