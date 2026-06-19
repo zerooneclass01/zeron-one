272ef15fb9b5ce476d5900e3bc332fd2de88bca6
Joao Guilherme Mathias Alves <joaoguilhermeoboista@gmail.com>
2026-06-18 23:54:32 -0300

    Correção

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   // Ajuste a porta (5000, 7000, etc) para a mesma que aparece no seu Swagger/C#
   apiUrl: 'https://zeroonecurso-001-site1.ltempurl.com/api', 
-  clientApiDriv:"844790086941-jga0bfl08nsre65iqcvtdb1648fa2hsq.apps.googleusercontent.com",
-  Apikey:"AIzaSyDT1zgo5hBEji6PEJ83ClLwP3YLrRWipEM"
+  clientApiDriv:'844790086941-jga0bfl08nsre65iqcvtdb1648fa2hsq.apps.googleusercontent.com',
+  Apikey:'AIzaSyDT1zgo5hBEji6PEJ83ClLwP3YLrRWipEM'
 };
